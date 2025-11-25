8cae51803700f4e955d67a9f296f93ad24f8ba92
CAWL ecommerce <ecommerce-integrations@cawl.com>
2025-11-25 14:07:13 +0100

    Add Pledg payment method, add translations
    ISSUE: WLDDEV-286

--- translations/es.php
+++ translations/es.php
@@ -128,6 +128,8 @@ $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_254ad5cf512e99709dbcb70646
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_ff76e4c4c9abdc7de325c97912ffb8cd'] = 'Recomendamos utilizar imágenes con un tamaño máximo de 20 píxeles de altura por 120 píxeles de anchura';
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_3c8990c77ed9e1634b5cdd133c6e0993'] = 'Título del botón de pago';
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_6f1ad8444a503e36d821da11b4b1fe57'] = 'Título del botón de selección de pago situado en tu página de pago';
+$_MODULE['<{cawlop}prestashop>_paymentmethodssettings_46310871a02a521ecfdfe034d1fd0085'] = 'Descriptor fijo';
+$_MODULE['<{cawlop}prestashop>_paymentmethodssettings_0b9f4e7ca03c57b2d6943e035e6c996e'] = 'Mínimo 2, máximo 15 caracteres. Puedes especificar un valor fijo que se enviará a los bancos que gestionan el pago. Si el banco del consumidor es compatible con esto, este valor puede mostrarse en el recibo bancario del consumidor. Nota importante: establecer un valor aquí es obligatorio para utilizar el método de pago Pledg.';
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_55e58b9f475a7a98a03b777f4e0a2ec7'] = 'Selección del método de pago antes del redireccionamiento';
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_6ef52d9cbb060e8e35b86085db5536cb'] = 'Actualiza la lista de métodos de pago disponibles';
 $_MODULE['<{cawlop}prestashop>_paymentmethodssettings_65640890ee8ab20a966bd445378bee4c'] = 'Nombre de archivo de la plantilla para el pago con redireccionamiento';
